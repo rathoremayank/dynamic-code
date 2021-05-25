2959947588811f8ea810ad78a188bb785f4704d1
root <root@cproject.driver.com>
2021-05-25 13:04:53 +0000

    changes 2

--- putvalues.sh
+++ putvalues.sh
@@ -1,9 +1,23 @@
 #!bin/bash
 
-variable=`cat properties/dev.properties | grep "envname" | cut -d'=' -f2`
+## Author: Mayank Rathore
+## Last revised 25/05/2021
 
-echo $variable
+echo "Starting to run putvalues.sh ..."
 
-sed -i "s/ENV_NAME/$variable/g" "src/main.txt"
+env_var=`cat properties/dev.properties | grep "envname" | cut -d'=' -f2`
+hostname_var=`cat properties/dev.properties | grep "hostname" | cut -d'=' -f2`
+
+echo "Below variables were read from the properties file.."
+echo "env_var: $env_var"
+echo "hostname_var: $hostname_var"
+
+
+sed -i "s/ENV_NAME/$env_var/g" "src/main.txt"
+sed -i "s/ENV_HOSTNAME/$hostname_var/g" "src/main.txt"
+
+
+echo "\nThis is the content of main.txt file after running the script.."
+echo "\n"
 
 cat src/main.txt
